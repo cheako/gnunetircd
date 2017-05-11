196d91eb5004281d1f335fd519668d16e2516d12
Travis CI <travis@travis-ci.org>
2017-05-11 12:45:34 +0000

    Deploy code docs to GitHub Pages Travis build: 111
    Commit: 2b1b9e31627b81049f3a6ca284ee208e371e1611

--- html/search/variables_a.js
+++ html/search/variables_a.js
@@ -1,7 +1,7 @@
 var searchData=
 [
+  ['name',['name',['../d1/db7/structBaseRoutingNode.html#a9ca3fefab591c949c7927805d3ebe083',1,'BaseRoutingNode']]],
   ['next',['next',['../df/d7a/structBaseRoutingNode_1_1WriteQueueItem.html#a40dfe04faa8ef1222f5e4c503df5a7bc',1,'BaseRoutingNode::WriteQueueItem::next()'],['../d0/dcd/structElement.html#a88551e77c4b5ed9586aeacf3afb2b404',1,'Element::next()']]],
   ['nhandle',['nhandle',['../df/d3d/structConnectionNode.html#a344b6dd896e4d23a8a13600bebf7c6dc',1,'ConnectionNode']]],
-  ['nick',['nick',['../d1/db7/structBaseRoutingNode.html#a24430cc470aeb6b8d51af3b092271304',1,'BaseRoutingNode']]],
   ['normal_5fcommands',['normal_commands',['../d7/d37/command__relay_8c.html#a92cb1c5b582c9524479c1b3509680642',1,'normal_commands():&#160;command_relay.c'],['../de/d98/command__relay_8h.html#a92cb1c5b582c9524479c1b3509680642',1,'normal_commands():&#160;command_relay.c']]]
 ];
