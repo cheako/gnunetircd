24e2eb235106f7749382c2db6578679cad4378e1
Travis CI <travis@travis-ci.org>
2017-05-11 17:33:26 +0000

    Deploy code docs to GitHub Pages Travis build: 112
    Commit: ffd31e51543f17ddf602477ff833f83b6f72f67d

--- html/search/defines_0.js
+++ html/search/defines_0.js
@@ -1,7 +1,9 @@
 var searchData=
 [
   ['gns_5fop_5fchan',['GNS_OP_CHAN',['../d0/d51/gns_8h.html#add8a59d8ca3bf9069265d47be059fc6d',1,'gns.h']]],
+  ['gns_5fop_5fhmac',['GNS_OP_HMAC',['../d0/d51/gns_8h.html#add579af82eaf200f98b9d26d428abc01',1,'gns.h']]],
   ['gns_5fop_5fnick',['GNS_OP_NICK',['../d0/d51/gns_8h.html#a8f868af3a022881170b57a1fb8a7f442',1,'gns.h']]],
+  ['gns_5fop_5fpbky',['GNS_OP_PBKY',['../d0/d51/gns_8h.html#a97cc3c738ab18a54a46e7adea486f76f',1,'gns.h']]],
   ['gns_5fop_5fprid',['GNS_OP_PRID',['../d0/d51/gns_8h.html#a462caaa868ed8e03b9432efa7666ddb6',1,'gns.h']]],
   ['gns_5fop_5freal',['GNS_OP_REAL',['../d0/d51/gns_8h.html#aac51baeb0efed23c7cb56373811eafca',1,'gns.h']]]
 ];
